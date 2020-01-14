33c97c8851327c6ea350bb7e7e417e9d33489b88
Diallo Mamadou <s185313@skinner.univ-lemans.fr>
2020-01-14 13:20:16 +0100

    ajout du resblock 2 et de la gru

--- modele_RawNet.py
+++ modele_RawNet.py
@@ -13,7 +13,7 @@ class RawNet(nn.Module):
         self.lrelu = nn.LeakyReLU()
         self.lrelu_keras = nn.LeakyReLU(negative_slope=0.3)
 
-        self.first_conv = nn.Conv1d(in_channels = 1,#1
+        self.conv1 = nn.Conv1d(in_channels = 1,#1
 			out_channels = 128,#128
 			kernel_size = 3,#3
                         padding = 0,
@@ -26,6 +26,8 @@ class RawNet(nn.Module):
 
         self.gru_fc1 = nn.Linear(1,1024)
         self.gru_fc2 = nn.Linear(1024,59049)
+        self.bn_before_gru = nn.BatchNorm1d(num_features = 256])
+
 
         self.gru2 = nn.Linear( in_features = 1, out_features = 1211)
 
@@ -36,19 +38,47 @@ class RawNet(nn.Module):
                                padding = 1,
                                stride = 1
         )
-        
+
+
         self.bn2 = nn.BatchNorm1d(num_features=128)
         
+        self.conv3_1 = nn.Conv1d(in_channels = 128,
+                               out_channels = 256,
+                               kernel_size = 3,
+                               padding = 1,
+                               stride = 1
+        )
+
+        self.conv3_2 = nn.Conv1d(in_channels = 256,
+                               out_channels = 256,
+                               kernel_size = 3,
+                               padding = 1,
+                               stride = 1
+        )
+
+        self.conv3_3 = nn.Conv1d(in_channels = 128,
+                               out_channels = 256,
+                               kernel_size = 1,
+                               padding = 0,
+                               stride = 1
+        )
+
+
+        self.bn3_1 = nn.BatchNorm1d(num_features=128)
+        self.bn3_2 = nn.BatchNorm1d(num_features=256)
+
+
         self.mp = nn.MaxPool1d(3)
 
     def forward(self, x):
-        out = self.first_conv(x)
+        out = self.conv1(x)
         out = self.bn(out)
         out = self.lrelu(out)
 
-        print("shape 1 : ", out.shape)
+        print("shape conv 1 : ", out.shape)
 
-        #--------Block 0--------------
+        #-------- Block 1 --------------
+        
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.lrelu_keras(out)
@@ -64,14 +94,68 @@ class RawNet(nn.Module):
         out = self.conv2(out)
         out = self.mp(out)
 
-        print("shape conv 2 : ", out.shape)
+        print("shape resblock 1 : ", out.shape)
 
-        out, _ = self.gru(x)
+        #-------- Block 2 --------------
+        
+        out_identite = out
+        out = self.conv3_1(out)
+        out = self.bn3_2(out)
+        out = self.lrelu_keras(out)
+        out = self.conv3_2(out)
+        out += self.conv3_3(out_identite)
+        out = self.mp(out)
+
+        out_identite = out
+        out = self.bn3_1(out)
+        out = self.lrelu_keras(out)
+        out = self.conv3_1(out)
+        out = self.bn3_2(out)
+        out = self.lrelu_keras(out)
+        out = self.conv3_2(out)
+        out += self.conv3_3(out_identite)
+        out = self.mp(out)
+
+        out_identite = out
+        out = self.bn3_1(out)
+        out = self.lrelu_keras(out)
+        out = self.conv3_1(out)
+        out = self.bn3_2(out)
+        out = self.lrelu_keras(out)
+        out = self.conv3_2(out)
+        out += self.conv3_3(out_identite)
+        out = self.mp(out)
+
+        out_identite = out
+        out = self.bn3_1(out)
+        out = self.lrelu_keras(out)
+        out = self.conv3_1(out)
+        out = self.bn3_2(out)
+        out = self.lrelu_keras(out)
+        out = self.conv3_2(out)
+        out += self.conv3_3(out_identite)
+        out = self.mp(out)
+
+        print("shape resblock 2 : ", out.shape)
+        
+
+        #-------- Gru --------------
+
+        out = self.bn_before_gru(out)
+        out = self.lrelu_keras(out)
+	out = out.permute(0, 2, 1)#(batch, filt, time) >> (batch, time, filt)
+
+        out, _ = self.gru(out)
         out = out[:,-1,:]
+        code = self.fc1_gru(out)
+        code_norm = code.norm(p=2,dim=1, keepdim=True) / 10.
+	code = torch.div(code, code_norm)
+	out = self.fc2_gru(code)
+
         out = self.gru_fc1(out)
         out = self.gru_fc2(out)
-        #print("shape : ",out.shape)
-        #print("shape : ",out.shape)
+
+        print("shape output : ", out)
 
         return out
 
@@ -85,9 +169,9 @@ def train(model, train_loader, optimizer, device):
 
         output = model(data)
         #print("output : ", output)
-        print("data shape : ", data.size())
+        #print("data shape : ", data.size())
         #print("target shape : ", target.size())
-        print("shape output : ",output.shape)
+        #print("shape output : ",output.shape)
 
         optimizer.zero_grad()
         loss = criterion(output,target)
